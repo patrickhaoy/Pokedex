62cdbcbf5eb7d076209a47f5c032c5e6f683a32f
Patrick Yin <patarhaoy@gmail.com>
2019-09-17 17:08:38 -0700

    table view of pokemon

---
+++ Pokedex/Controllers/SearchVC/SearchVC-TableView.swift
@@ -0,0 +1,13 @@
+//
+//  SearchVC-TableView.swift
+//  Pokedex
+//
+//  Created by Patrick Yin on 9/16/19.
+//  Copyright © 2019 Patrick Yin. All rights reserved.
+//
+
+import UIKit
+
+class SearchVC_TableView: UITableViewDataSource {
+
+}
